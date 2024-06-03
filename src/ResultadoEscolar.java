535827e5f23e1e5c45cac1c88531f288617c75cc
Rafaely RP <rafarprocopio@gmail.com>
2024-06-03 15:07:54 -0300

    alterações e atualizações para aula de estruturas condicionais

---
+++ src/ResultadoEscolar.java
@@ -0,0 +1,28 @@
+public class ResultadoEscolar {
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+      int nota = 6;
+       
+    if(nota >= 7){
+     System.out.println("Aprovado");
+
+    }else if (nota >= 5 && nota < 7){
+     System.out.println("Prova de Recuperação");
+     
+    }else{
+     System.out.println("Reprovado");
+    }
+
+
+    //nova forma melhorada de escrever
+    String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+    System.out.println(resultado);
+
+    //nova segunda forma melhorada de escrever
+    String resultado = nota >=7 ? "Aprovado" : nota >=5 && nota <7 ? "Recuperação" : "Reprovado";
+    System.out.println(resultado);
+    
+    }    
+}
