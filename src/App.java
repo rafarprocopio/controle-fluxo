df426ad4e8f2015c3b5e91f56047cb18d64715d4
Rafaely RP <rafarprocopio@gmail.com>
2024-06-04 07:38:32 -0300

    alterações trilha java basico- tratamento de exceções

--- src/App.java
+++ src/App.java
@@ -1,13 +1,6 @@
 public class App {
     public static void main(String[] args) {
-        double alturaJoao = 1.46;
-        double alturaMarcos = 1.37;
-
-       if(alturaMarcos > alturaJoao)
-        System.out.println("João é mais alto que Marcos");
-       else
-        System.out.println("Marcos é mais baixo que João");
-        
+              
     }
     
 }
